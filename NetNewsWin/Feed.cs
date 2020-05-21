f7d587da2d58fe7db662b0aaef10723881fcd2a9
Reilly Wood <reilly.wood@icloud.com>
2020-05-20 21:23:46 -0700

    Add feed unread count

--- NetNewsWin/Feed.cs
+++ NetNewsWin/Feed.cs
@@ -1,25 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Specialized;
 
 namespace NetNewsWin
 {
-    public class Feed
+    public class Feed : INotifyPropertyChanged
     {
         public string Name { get; private set; }
         public ObservableCollection<Entry> Entries { get; private set; } = new ObservableCollection<Entry>();
 
-        public Feed(string name)
+        public int UnreadCount => Entries.Count(e => e.Unread);
+
+        public Feed(string name, IEnumerable<Entry> entries)
         {
             Name = name;
+            Entries = new ObservableCollection<Entry>(entries);
+
+            foreach (var entry in Entries)
+            {
+                entry.PropertyChanged += Entry_PropertyChanged;
+            }
+
+            Entries.CollectionChanged += Entries_CollectionChanged;
         }
 
-        public Feed(string name, IEnumerable<Entry> entries) : this(name)
+        private void Entries_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            Entries = new ObservableCollection<Entry>(entries);
+            if (e.OldItems != null)
+            {
+                foreach (INotifyPropertyChanged entry in e.OldItems)
+                    entry.PropertyChanged -= Entry_PropertyChanged;
+            }
+            if (e.NewItems != null)
+            {
+                foreach (INotifyPropertyChanged entry in e.NewItems)
+                    entry.PropertyChanged += Entry_PropertyChanged;
+            }
         }
+
+        private void Entry_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(Entry.Unread))
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UnreadCount)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
